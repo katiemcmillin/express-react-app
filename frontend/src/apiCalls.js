b0bd970a993a875a95c921fe5cd2d8be2b68ae15
katiemcmillin <katiemcmillin@gmail.com>
2023-05-09 23:53:09 -0400

    add backend

--- frontend/src/apiCalls.js
+++ frontend/src/apiCalls.js
@@ -1,6 +1,6 @@
 // BASE URL OF OUR DEPLOYED API
 const URL = process.env.REACT_APP_NODE_ENV === "production"
-? "https://express-react-lab-allison2.onrender.com/"
+? "https://express-react-lab-allison2.onrender.com"
 : "http://localhost:4000";
 
 export const projectsLoader = async () => {
